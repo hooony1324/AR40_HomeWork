8b52194d7b68e467c3c8010ce03084caa0ccb15c
Cogason <hooony1324@gmail.com>
2022-01-13 14:37:58 +0900

    김성훈 : HomeWork220113 커밋

---
+++ HomeWork/HomeWork220113/HomeWork220113.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+void StringTrimRemove(const char* _Left, char* _Right)
+{
+    int LeftIndex = 0;
+    int RightIndex = 0;
+    while (_Left[LeftIndex] != 0)
+    {
+        if (_Left[LeftIndex] == ' ')
+        {
+
+        }
+        else
+        {
+            _Right[RightIndex] = _Left[LeftIndex];
+            RightIndex += 1;
+        }
+
+        LeftIndex += 1;
+    }
+
+    _Right[RightIndex + 1] = 0;
+    
+}
+
+int main()
+{
+    char Text[10000] = { 0, };
+    StringTrimRemove("a b ,<.>/?:;! ~` @##$ %%^ &() *_+-=  1 2 3 c d", Text);
+    // "aaabbbcccddd"
+    printf_s(Text);
+}
