71fb2cdcef7c60794e4ae783bcad02412eb65729
hooony1324 <42611599+hooony23@users.noreply.github.com>
2022-01-14 16:52:17 +0900

    김성훈 : HomeWork220114 커밋

---
+++ HomeWork/HomeWork220114/HomeWork220114.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+// 좀 어려울거다.
+int ToInt(const char* _Left)
+{
+	// 문자열 길이 측정
+	int zeros = 0;
+	int leftIndex = 0;
+	while (_Left[leftIndex] != 0)
+	{
+		leftIndex += 1;
+	}
+	zeros = leftIndex - 1;
+
+	// 문자의 자릿수 별로 계산
+	int result = 0;
+	for (int i = 0; i < leftIndex; i += 1)
+	{
+		int tens = 1;
+		for (int j = 0; j < zeros; j += 1)
+		{
+			tens *= 10;
+		}
+
+		result += ((int)(_Left[i] - 48)) * tens;
+		zeros -= 1;
+	}
+
+	return result;
+}
+
+int main()
+{
+
+	{
+		char Text = '1';
+		int Count = '2';
+
+		int Value = ToInt("2147483647");
+
+		int a = 0;
+	}
+
+}
