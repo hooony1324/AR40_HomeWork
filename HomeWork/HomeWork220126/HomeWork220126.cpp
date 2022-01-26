4606032a385cf326ec9b222dfcfcb48138dd4cce
hooony1324 <42611599+hooony23@users.noreply.github.com>
2022-01-26 20:06:14 +0900

    김성훈 : HomeWork220126 커밋

---
+++ HomeWork/HomeWork220126/HomeWork220126.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+class MyInt
+{
+public:
+    int Value;
+
+public:
+
+    MyInt(int _Value)
+        : Value(_Value)
+    {
+
+    }
+
+    // ++MyInt -> 1증가된 값 리턴
+    // MyInt++ -> 값 리턴 후 1 증가
+
+    // ++MyInt
+    MyInt& operator++()
+    {
+        this->Value += 1;
+        return *this;
+    }
+
+    // MyInt++
+    MyInt operator++(int _Other)
+    {
+        MyInt temp = MyInt(this->Value);
+        this->Value += 1;
+        return temp;
+    }
+
+};
+
+int main()
+{
+    int Value = 0;
+    int Result = 0;
+
+    Result = ++Value;
+    Result = 0;
+    Result = Value++;
+
+    MyInt MyValue = 0;
+    MyInt MyResult = 0;
+
+    // ++ 쓰지마세요
+    MyResult = ++MyValue;
+    MyResult = 0;
+    MyResult = MyValue++;
+
+    int a = 0;
+}
