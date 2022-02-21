0463e7c43dacc7e385f668d9e6644c4f2bf93fae
Cogason <hooony1324@gmail.com>
2022-02-21 22:54:52 +0900

    김성훈 : HomeWork220221 - Snake게임 Leak해결 커밋

---
+++ HomeWork/HomeWork220221/HomeWork220221.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include "Engine/ConsoleScreen.h"
+#include <conio.h>
+#include "Head.h"
+#include "Body.h"
+#include <vector>
+#include "Engine/ConsoleRandom.h"
+#include "GlobalGameLogic.h"
+
+int main()
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	ConsoleScreen::GetInst().CreateScreen(10, 10, "□");
+
+	ConsoleObject* NewHead = new Head();
+	NewHead->Init({ 5, 5 }, "★");
+
+
+	ConsoleObject* NewBody = GlobalGameLogic::CreateBodyLogic(NewHead);
+
+	while (true)
+	{
+		if (nullptr == NewBody)
+		{
+			NewBody = GlobalGameLogic::CreateBodyLogic(NewHead);
+		}
+
+		ConsoleScreen::GetInst().RenderStart();
+
+		NewBody->Render();
+		NewHead->Render();
+
+		ConsoleScreen::GetInst().RenderEnd();
+
+		NewBody->Update();
+		NewHead->Update();
+
+		if (true == NewHead->GetIsDeath())
+		{
+			break;
+		}
+
+		if (NewHead->OverLapCheck(NewBody))
+		{
+			NewHead->OverLap(NewBody);
+			NewBody = nullptr;
+		}
+	}
+
+	if (nullptr != NewBody)
+	{
+		delete NewBody;
+		NewBody = nullptr;
+	}
+
+	if (nullptr != NewHead)
+	{
+		delete NewHead;
+		NewHead = nullptr;
+	}
+
+	ConsoleScreen::Destroy();
+}
