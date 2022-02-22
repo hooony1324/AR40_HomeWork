b37c83dbd4a569f5d87f3004ef3be7d7f8c6cf6f
hooony1324 <42611599+hooony23@users.noreply.github.com>
2022-02-22 20:13:32 +0900

    김성훈 : HomeWork220222 - GameVectorSort 커밋

---
+++ HomeWork/HomeWork220222/HomeWork220222.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <map>
+#include "GameVector.h"
+
+GameVector<int> ReturnVector()
+{
+    GameVector<int> NewArr;
+
+    NewArr.resize(20);
+
+    return NewArr;
+}
+
+int main()
+{
+    {
+        GameVectorSort<int> MyVector = GameVectorSort<int>();
+
+        MyVector.push_back(5);
+        MyVector.push_back(10);
+        MyVector.push_back(5);
+        MyVector.push_back(1);
+        MyVector.push_back(2);
+        MyVector.push_back(8);
+        MyVector.push_back(99);
+
+        for (size_t i = 0; i < MyVector.size(); i++)
+        {
+            std::cout << MyVector[i] << std::endl;
+        }
+    }
+}
