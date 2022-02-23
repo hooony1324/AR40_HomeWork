9aeb851f5a551bc45bd53d85ab86eb770cfeb564
Cogason <hooony1324@gmail.com>
2022-02-23 16:02:17 +0900

    김성훈 : HomeWork220223 - 재귀함수 종료숙제 - 커

---
+++ HomeWork/HomeWork220223/HomeWork220223.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+// 
+
+int Value = 0;
+
+void Func()
+{
+    if (Value == 30)
+    {
+        return;
+    }
+
+    std::cout << ++Value << std::endl;
+    Func();
+}
+
+int main()
+{
+    Func();
+}
